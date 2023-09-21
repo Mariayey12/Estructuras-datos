351d5970122d19e25117973ecdefa2b034266b21
Maria Martinez <mmariayennifer.25@gmail.con>
2023-09-20 23:51:52 -0500

    Estructuras de datos lista enlazada

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,173 @@
-package PACKAGE_NAME;public class LinkedList {
+public class LinkedList {
+    class  Node {
+        int value;
+        Node next = null;
+        Node(int value){
+            this.value = value;
+        }
+    }
+    protected Node  head = null;
+    protected Node tail = null;
+
+    // [5] ->  next null  , head [5] tail [5]
+    public void agregarHead(int valor) {
+        Node nuevoNodo = new Node(valor);
+        nuevoNodo.next = head;
+        head = nuevoNodo;
+        if(nuevoNodo.next == null){
+            tail = nuevoNodo;
+        }
+    }
+
+    public void agregarTail(int valor) {
+        Node nuevoNodo = new Node(valor);
+        if (tail == null){
+            head = nuevoNodo;
+            tail = nuevoNodo;
+        } else {
+            tail.next = nuevoNodo;
+            tail = nuevoNodo;
+        }
+    }
+
+    public void agregarPorPosicion(int posicion, int valor) {
+        if (posicion < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if (posicion == 0) {
+            agregarHead(valor);
+        }else {
+            Node node = new Node(valor);
+            Node actual = head;
+            for (int i= 0; i < posicion-1; i++) {
+                if(actual == null) {
+                    throw new IndexOutOfBoundsException();
+                }
+                actual = actual.next;
+            }
+            //[0] -> [3] -> [5] -> [10]
+            if(actual.next == null) {
+                agregarTail(valor);
+            } else {
+                //[0] -> [3] -> [99]->[5]  -> [10]
+                node.next = actual.next;
+                actual.next = node;
+            }
+        }
+    }
+
+    //1- Buscar por posición //  [5] -> [10] -> Null//  [25] -> [10] -> Null
+    public int buscarPorPosicion(int posicion) {
+        if (posicion < 0) {
+            throw new IndexOutOfBoundsException("La posición no puede ser negativa");
+        } else if (head == null) {
+            throw new IndexOutOfBoundsException("La lista está vacía");
+        } else {
+            Node actual = head;
+            for (int i = 0; i < posicion; i++) {
+                if (actual == null) {
+                    throw new IndexOutOfBoundsException("La posición está fuera de rango");
+                }
+                actual = actual.next;
+            }
+            if (actual == null) {
+                throw new IndexOutOfBoundsException("La posición está fuera de rango");
+            }
+            return actual.value;
+        }
+    }
+    // 2- Eliminar por posición
+    public void eliminarPorPosicion(int posicion) {
+        if (posicion < 0) {
+            throw new IndexOutOfBoundsException("La posición no puede ser negativa");
+        } else if (head == null) {
+            throw new IndexOutOfBoundsException("La lista está vacía");
+        } else if (posicion == 0) {
+            head = head.next;
+            if (head == null) {
+                tail = null; // Si la lista queda vacía, también actualizamos la cola.
+            }
+        } else {
+            Node actual = head;
+            Node anterior = null;
+            for (int i = 0; i < posicion; i++) {
+                if (actual == null) {
+                    throw new IndexOutOfBoundsException("La posición está fuera de rango");
+                }
+                anterior = actual;
+                actual = actual.next;
+            }
+            if (actual == null) {
+                throw new IndexOutOfBoundsException("La posición está fuera de rango");
+            }
+            anterior.next = actual.next;
+            if (anterior.next == null) {
+                tail = anterior; // Si eliminamos el último elemento, actualizamos la cola.
+            }
+        }
+    }
+
+    //3- Contiene un valor
+    public boolean contieneValor(int valor) {
+        Node actual = head;
+        while (actual != null) {
+            if (actual.value == valor) {
+                return true;
+            }
+            actual = actual.next;
+        }
+        return false;
+    }
+    //4- Función para reemplazar un nodo con un valor específico
+    public static void reemplazarNodo(LinkedList linkedList, int valorAntiguo, int valorNuevo) {
+        LinkedList.Node nodoActual = linkedList.head;
+        while (nodoActual != null) {
+            if (nodoActual.value == valorAntiguo) {
+                nodoActual.value = valorNuevo; // Reemplazar el valor antiguo con el nuevo valor
+                break; // Terminar la búsqueda una vez que se haya realizado el reemplazo
+            }
+            nodoActual = nodoActual.next;
+        }
+    }
+
+    // 5-Función para recorrer e imprimir la lista completa con un ciclo for
+    public static void recorrerYImprimir(LinkedList linkedList) {
+        LinkedList.Node nodoActual = linkedList.head;
+        for (; nodoActual != null; nodoActual = nodoActual.next) {
+            System.out.print(nodoActual.value + " -> ");
+        }
+        System.out.println("null"); // Agregar "null" al final para indicar el final de la lista
+    }
+
+
+
+    //6- Eliminar al Inicio
+    public void eliminarAlInicio() {
+     if (head != null) {
+        head = head.next;
+     if (head == null) {
+        tail = null; // Si la lista queda vacía, también actualizamos la cola.
+       }
+     }
+    }
+
+    //-7 Eliminar al Final
+    public void eliminarAlFinal() {
+        if (head != null) {
+            if (head == tail) {
+                head = null;
+                tail = null; // Si solo hay un elemento en la lista, eliminamos tanto la cabeza como la cola.
+            } else {
+                Node nodoActual = head;
+                while (nodoActual.next != tail) {
+                    nodoActual = nodoActual.next;
+                }
+                nodoActual.next = null;
+                tail = nodoActual; // Actualizamos la cola para que apunte al último nodo restante.
+            }
+        }
 }
+}
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
+import jdk.dynalink.beans.StaticClass;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Agregar elementos al principio (agregarHead)
+
+        LinkedList linkedList = new LinkedList();
+        linkedList.agregarHead(5);
+        linkedList.agregarHead(3);
+        linkedList.agregarHead(0);
+
+        // Agregar elementos al final (agregarTail)
+        linkedList.agregarTail(10);
+        linkedList.agregarTail (20);
+        linkedList.agregarTail (30);
+
+        // Agregar elemento en posición específica (agregarPorPosicion)
+        linkedList.agregarPorPosicion(2, 99);
+        linkedList.agregarPorPosicion(2, 15); // Insertar 15 en la posición 2
+
+        // Buscar y mostrar elementos por posición
+        System.out.println("Elemento en posición 0: " + linkedList.buscarPorPosicion(0));
+        System.out.println("Elemento en posición 3: " + linkedList.buscarPorPosicion(3));
+        // Verificar si la lista contiene ciertos valores
+        System.out.println("¿Contiene el valor 10? " + linkedList.contieneValor(10));
+        System.out.println("¿Contiene el valor 25? " + linkedList.contieneValor(25));
+        // Eliminar elementos por posición
+        linkedList.eliminarPorPosicion(1); // Eliminar el elemento en la posición 1
+
+
+        // Eliminar al inicio
+        linkedList.eliminarAlInicio();
+        // Eliminar al final
+        linkedList.eliminarAlFinal();
+
+      // Llamar a la función para reemplazar un nodo (por ejemplo, reemplazar 5 con 25)
+        linkedList.reemplazarNodo (linkedList,5,25);
+        // Recorrer e imprimir la lista completa
+    linkedList.recorrerYImprimir(linkedList);
+
+        // Mostrar la lista completa con un bucle for
+        LinkedList.Node nodoActual = linkedList.head;
+        for (; nodoActual != null; nodoActual = nodoActual.next) {
+            System.out.print(nodoActual.value + " -> ");
+        }
     }
 }
+
+
+
+
+// [7] -> [6] -> [5]
+// [7] -> [0] -> [5] -> [10]
+// [7] -> [0] -> [1] ->  [5] -> [10]
+// [7] -> [0] -> [1] ->  [5] -> [10] -> [6] -> [8]
+// [7] -> [0] -> [1] ->  [5] -> [10] -> [8] ->  [6] -> [8]
+//agrege un nodo en la posicion  = 5;
+// head                 //tail
+
+
+
